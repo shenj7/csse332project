71e3c00a17ac2cb00176cb356de31ee6a1827b3d
Mohammad <noureddi@rose-hulman.edu>
2022-02-13 23:22:50 -0500

    added list code for milestone 2

--- kernel/proc.h
+++ kernel/proc.h
@@ -82,6 +82,10 @@ struct trapframe {
 
 enum procstate { UNUSED, USED, SLEEPING, RUNNABLE, RUNNING, ZOMBIE };
 
+struct list_head {
+  struct list_head *next, *prev;
+};
+
 // Per-process state
 struct proc {
   struct spinlock lock;
